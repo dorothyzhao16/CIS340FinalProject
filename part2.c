01cf01997f6fabc16bf47d8d0d7bd1d7fab3bc18
Yiyuan Dorothy Zhao <zhao.dorothy33@gmail.com>
2018-04-29 22:05:09 -0400

    Create alternative code for part 2 of project
    Utilizing disPerm, argc and argv, 
    
    gcc part2.c -o part 2
    ./part2 /home/...whatever path you wanna take

---
+++ part2.c
@@ -0,0 +1,68 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+void disPerm(char file[]) {
+
+	struct stat fileStat;
+	lstat(file, &fileStat);
+	//printf("File permissions: \t");
+	printf((S_ISDIR(fileStat.st_mode)) ? "d":"-");
+	printf((fileStat.st_mode & S_IRUSR) ? "r":"-");
+	printf((fileStat.st_mode & S_IWUSR) ? "w":"-");
+	printf((fileStat.st_mode & S_IXUSR) ? "x":"-");
+	printf((fileStat.st_mode & S_IRGRP) ? "r":"-");
+	printf((fileStat.st_mode & S_IWGRP) ? "w":"-");
+	printf((fileStat.st_mode & S_IXGRP) ? "x":"-");
+	printf((fileStat.st_mode & S_IROTH) ? "r":"-");
+	printf((fileStat.st_mode & S_IWOTH) ? "w":"-");
+	printf((fileStat.st_mode & S_IXOTH) ? "x":"-");
+
+	printf("\t%ld",fileStat.st_nlink);
+	//printf("\t%d",fileStat.st_uid);
+	printf("\t%ld",fileStat.st_size);
+	
+	printf("\t%ld",fileStat.st_atime);
+	printf("\t%s",file);
+	printf("\n");
+}
+
+int main(int argc, char *argv[]) {
+
+	/*
+	//pointer for dir entry
+	struct dirent *de;
+	DIR *dr = opendir(argv[1]);
+
+	if(dr == NULL) { //opendir returns null if it couldn't open dir
+		printf("Could not open %s directory", argv[1]);
+		return 0;
+	}
+
+	while((de = readdir(dr)) != NULL) {
+		char f[50];
+		strcpy(f,de->d_name);
+		disPerm(f);
+	}
+
+	closedir(dr); //close directory		
+
+
+	return 0;*/
+
+	if(argc == 1) 
+		disPerm(".");
+	else {
+		while(--argc) {
+			printf("%s:\n", *++argv);
+			disPerm(*argv);
+		}
+	}
+
+}
